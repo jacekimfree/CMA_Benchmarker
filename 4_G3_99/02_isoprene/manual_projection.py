c77d44775abc0221a1175800f3f174551399c90b
Jace Jin <jj04645@vlogin2.ccqc.uga.edu>
2024-09-24 15:10:59 -0400

    02 benchmarked

--- 4_G3_99/02_isoprene/manual_projection.py
+++ 4_G3_99/02_isoprene/manual_projection.py
@@ -16,34 +16,66 @@ class Projection(object):
 
     def run(self):
 
-        unc = np.eye(1)
-
-        ch_2str = normalize(np.array([
-            [1, 1],
-            [1, -1]
-        ]).T)
-
-        ch3_str = normalize(np.array([
+        # 0-2
+        str_c4_c2 = normalize(np.array([
             [1, 1, 1],
             [2, -1, -1],
             [0, 1, -1]
         ]).T)
 
-        cc3_plain_ang = normalize(np.array([
-            [2, -1, -1],
-            [0, 1, -1]
-        ]).T)
+        # 3
+        str_c3c4 = np.eye(1)
 
-        ch_ang = normalize(np.array([
+        # 4
+        str_c3c8 = np.eye(1)
+
+        # 5-6
+        str_ch2_c1 = normalize(np.array([
+            [1, 1],
             [1, -1]
         ]).T)
 
-        ch2c_ang = normalize(np.array([
+        # 7-8
+        str_ch2_c4 = normalize(np.array([
+            [1, 1],
+            [1, -1]
+        ]).T)
+
+        # 9-11
+        str_ch3_c5 = normalize(np.array([
+            [1, 1, 1],
             [2, -1, -1],
             [0, 1, -1]
         ]).T)
 
-        ch3_ang = normalize(np.array([
+        # 12
+        ang_c2c3c4 = np.eye(1)
+
+        # 13-14
+        ang_c4_c2 = normalize(np.array([
+            [2, -1, -1],
+            [0, 1, -1]
+        ]).T)
+
+        # 15
+        ang_ch_c3 = normalize(np.array([
+            [1, -1]
+        ]).T)
+
+        # 16-17
+        ang_ch2c_c4 = normalize(np.array([
+            [2, -1, -1],
+            [0, 1, -1]
+        ]).T)
+
+        # 18-19
+        ang_ch2c_c1 = normalize(np.array([
+            [2, -1, -1],
+            [0, 1, -1]
+        ]).T)
+
+        # 20-24
+        ang_ch3_c5 = normalize(np.array([
             [1, 1, 1, -1, -1, -1],
             [2, -1, -1, 0, 0, 0],
             [0, 1, -1, 0, 0, 0],
@@ -51,31 +83,51 @@ class Projection(object):
             [0, 0, 0, 0, 1, -1]
         ]).T)
 
-        ccc3_rot = normalize(np.array([
+        # 25
+        rot_c1c5_c2c3 = normalize(np.array([
             [1, 1]
         ]).T)
 
-        ch2_rot = normalize(np.array([
+        # 26
+        rot_ch2_c1 = normalize(np.array([
             [1, 1]
         ]).T)
 
-        ch3_rot = normalize(np.array([
+        # 27
+        rot_ch2_c4 = normalize(np.array([
+            [1, 1]
+        ]).T)
+
+        # 28
+        rot_ch3_c5 = normalize(np.array([
             [1, 1, 1]
         ]).T)
 
-        cc3_plain_oop = normalize(np.array([
+        # 29
+        oop_c2 = normalize(np.array([
             [1, 1, 1]
         ]).T)
 
-        Proj = block_diag(ch3_str, unc, unc, ch_2str, ch_2str, ch3_str, unc, cc3_plain_ang, ch_ang,
-                          ch2c_ang, ch2c_ang, ch3_ang, ccc3_rot, ch2_rot, ch2_rot, ch3_rot, cc3_plain_oop, unc, unc, unc)
-'''
-        self.sym_sort = np.array([
-            [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13,],
-            [4, 12, 14, 15, 16, 17, 18]
-            ],dtype=object)
-'''
+        # 30
+        oop_c4 = np.eye(1)
+
+        # 31
+        oop_c1 = np.eye(1)
+
+        # 32
+        oop_c3 = np.eye(1)
+
+        Proj = block_diag(str_c4_c2, str_c3c4, str_c3c8, str_ch2_c1, str_ch2_c4, str_ch3_c5, ang_c2c3c4, ang_c4_c2, ang_ch_c3,
+                          ang_ch2c_c4, ang_ch2c_c1, ang_ch3_c5, rot_c1c5_c2c3, rot_ch2_c1, rot_ch2_c4, rot_ch3_c5, oop_c2, oop_c4, oop_c1, oop_c3)
+
         self.Proj = Proj
+
+        self.sym_sort = np.array([
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13,
+                14, 15, 16, 17, 18, 19, 20, 21, 23],
+            [11, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+        ], dtype=object)
+
 
 def normalize(mat):
     return 1/norm(mat, axis=0)*mat
